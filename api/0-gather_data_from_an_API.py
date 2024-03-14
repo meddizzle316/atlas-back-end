b2804df2a985037c5eb74e7a5062f3b53124eb2f
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-03-14 15:30:54 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,9 +9,49 @@ base_url = "https://jsonplaceholder.typicode.com/"
 #getting Employee name using params passed to file
 id = sys.argv[1]
 #making sure I got the right id
-print(id)
+# print(id)
 
 #requesting user with given id and converting it to json object
 employee = requests.get(base_url + "users/" + id)
 j_employee = employee.json()
-print(j_employee)
+
+#print out json employee object to make sure it worked
+# print(j_employee)
+
+#using "get" on json object to get name field
+EMPLOYEE_NAME:str = j_employee.get("name")
+
+#checking previous operation
+# print(EMPLOYEE_NAME)
+
+# requesting todos, converting to json object
+todos = requests.get(base_url + "todos/")
+j_todos = todos.json()
+
+# printing json object
+# print(j_todos)
+#initializing count 
+total_tasks = 0
+
+#getting total tasks assigned to employee
+for task in j_todos:
+    if task["userId"] == int(id):
+        total_tasks += 1
+
+#checking total_tasks operation
+# print(total_tasks)
+
+
+#initializing count
+completed_tasks = 0
+completed_task_list = []
+
+for task in j_todos:
+    if task["userId"] == int(id) and task["completed"] == True:
+        completed_tasks += 1
+        completed_task_list.append(task["title"])
+
+completed_task_list.insert(0, f"Employee {EMPLOYEE_NAME} is done with tasks({completed_tasks}/{total_tasks}):")
+
+# print(completed_tasks)
+print(*completed_task_list, sep='\n\t')
